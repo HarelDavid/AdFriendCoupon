1eadd720670f997a69b6a014898c1d852438b3a4
HarelDavid <hdavid@aptrinsic.com>
2017-10-21 16:21:03 +0300

    Fix

--- src/client/containers/pages/ThankYou.jsx
+++ src/client/containers/pages/ThankYou.jsx
@@ -5,9 +5,18 @@ import { observer } from 'mobx-react'
 
 export default class ThankYou extends React.Component {
 
-    render() {
-        return <section>
-                Thank you
-        </section>
-    }
+
+        render() {
+
+                return <div className="Coupon">
+                    <div className="Coupon-inner">
+                        <div>
+                            <h1>תודה רבה</h1>
+                            <p>נציגינו יצרו איתך קשר בהקדם</p>
+
+                        </div>
+                    </div>
+                </div>
+            }
+
 }
